5a7d4d6dd091de9c5ecd5537cf0df05a03984c47
testautomation-01-2018 <automation@wp.pl>
2018-11-22 19:50:12 +0100

    Basket, Item, remove BasketItem, add Lombok

--- src/main/java/com/pragmaticcoders/checkout/model/Basket.java
+++ src/main/java/com/pragmaticcoders/checkout/model/Basket.java
@@ -2,35 +2,46 @@ package com.pragmaticcoders.checkout.model;
 
 import io.swagger.annotations.ApiModel;
 import io.swagger.annotations.ApiModelProperty;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+import lombok.ToString;
 import org.hibernate.validator.constraints.Length;
 
 import javax.persistence.*;
 import javax.validation.constraints.NotNull;
 import java.math.BigDecimal;
+import java.util.HashSet;
+import java.util.LinkedHashSet;
 import java.util.Set;
 
 @ApiModel
 @Entity
-@Table(name = "baskets")
+@Table(name = "basket")
+@Data
+@NoArgsConstructor
 public class Basket {
 
-    @ApiModelProperty(position = 1, required = true, notes = "The database generated product ID")
+    @ApiModelProperty(position = 1, dataType = "long", required = true, notes = "The database generated product ID")
     @Id
     @GeneratedValue(strategy=GenerationType.AUTO)
-    private Long id;
-
+    private Long basket_id;
 
     @Column(name = "status")
     @NotNull(message = "Status can not be empty")
-    @ApiModelProperty(position = 3, required = true, notes = "basket status")
+    @ApiModelProperty(position = 2, dataType = "BasketStatus", allowableValues = "NEW, ACTIVE, CLOSED, CANCELED", required = true, notes = "basket status")
     private BasketStatus status;
 
     @Column(name = "totalPrice")
     @NotNull(message = "Total price can not be empty")
-    @ApiModelProperty(position = 3, required = true, notes = "Total price")
+    @ApiModelProperty(position = 3, dataType = "BigDecimal", required = true, notes = "Total price")
     private BigDecimal totalPrice;
 
-    @OneToMany(mappedBy="basket")
-    private Set<Item> items;
+    @ManyToMany(mappedBy="basket")
+    @JoinTable(
+            name = "basket_item",
+            joinColumns = { @JoinColumn(name = "basket_id") },
+            inverseJoinColumns = { @JoinColumn(name = "item_id") }
+    )
+    private Set<Item> items = new LinkedHashSet<>();
 
 }
--- src/main/java/com/pragmaticcoders/checkout/model/Item.java
+++ src/main/java/com/pragmaticcoders/checkout/model/Item.java
@@ -2,33 +2,43 @@ package com.pragmaticcoders.checkout.model;
 
 import io.swagger.annotations.ApiModel;
 import io.swagger.annotations.ApiModelProperty;
+import lombok.Data;
+import lombok.NoArgsConstructor;
 
 import javax.persistence.*;
 import javax.validation.constraints.NotNull;
 import java.math.BigDecimal;
+import java.util.LinkedHashSet;
+import java.util.Set;
 
 @ApiModel
 @Entity
-@Table(name = "items")
+@Table(name = "item")
+@Data
+@NoArgsConstructor
 public class Item {
 
-    @ApiModelProperty(position = 1, required = true, notes = "The database generated product ID")
+    @ApiModelProperty(position = 1, dataType = "Long", required = true, notes = "The database generated product ID")
     @Id
     @GeneratedValue(strategy=GenerationType.AUTO)
     private Long id;
 
     @Column(name = "type")
     @NotNull(message = "Type can not be empty or null")
-    @ApiModelProperty(position = 2, required = true, notes = "type")
+    @ApiModelProperty(position = 2, dataType = "String", required = true, notes = "type")
     private String productType;
 
     @Column(name = "price")
     @NotNull(message = "Price can not be empty")
-    @ApiModelProperty(position = 3, required = true, notes = "price")
+    @ApiModelProperty(position = 3, dataType = "BigDecimal", required = true, notes = "price")
     private BigDecimal price;
 
-    @ManyToOne
+    @Column(name = "quantity")
+    @ApiModelProperty(position = 4, dataType = "int", required = true, notes = "quantity")
+    private int quantity;
+
+    @ManyToMany(mappedBy = "items")
     @JoinColumn(name="id", nullable=false)
-    private Basket basket;
+    private Set<Basket> baskets = new LinkedHashSet<>();
 
 }
